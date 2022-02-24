838d933694646a7c58c1b6a2d6c067ba1631c464
riadulIslam008 <riadulislam088@gmail.com>
2022-02-24 18:28:09 +0600

    first commit

--- lib/Core/app_string.dart
+++ lib/Core/app_string.dart
@@ -1,10 +1,10 @@
 class AppString {
   static const List puzzleLengthList = <String>[
+    "5x5",
     "6x6",
     "7x7",
     "8x8",
     "9x9",
-    "10x10"
   ];
 
   static const String dbName = "ImageStore";
